9065632497e7620740ae2065fd17a3b4e2268d85
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-01-28 23:35:19 -0500

    sdfg
    sfg

--- comp/page_1_intro/app.js
+++ comp/page_1_intro/app.js
@@ -13,12 +13,19 @@ $(document).ready(function () {
 });
 
 
-    function init_s0() {
-        
-        var options = {
-            strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-            typeSpeed: 40
-        }
+function init_s0() {
 
-        var typed = new Typed("#typejs_s0_t1", options);
-    }
+    var typed = new Typed("#typejs_s0_t1", {
+        strings: ['Hola! ,^500 somos <span class="f-bold">TheSign Lab</span>'],
+        typeSpeed: 30
+    });
+
+}
+
+
+function init_s0_p(){
+        var typed2 = new Typed("#typejs_s0_t2", {
+        strings: ['un laboratorio de diseño con proyectos innovadores desarrollados a partir de iniciativas en multiples campos creativos como el branding estratégico, diseño y desarrollo de producto.'],
+        typeSpeed: 30
+    });
+}
